a24abf058e556065011ec1793436f2fb0c67c40a
anhducmata <anhducmata@gmail.com>
2017-03-10 13:41:05 +0700

    header create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,29 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="plugins/morris/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    @yield('customjs')
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
@@ -26,13 +48,7 @@
             <div class="container">
                 <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
@@ -52,37 +68,176 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Please Login !</a></li>
                         @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <header class="main-header">
+                        <!-- Header Navbar -->
+                        <nav class="navbar navbar-static-top" role="navigation">
+                          <!-- Sidebar toggle button-->
+                            <span class="sr-only">Toggle navigation</span>
+                          </a>
+                          <!-- Navbar Right Menu -->
+                          <div class="navbar-custom-menu">
+                            <ul class="nav navbar-nav">
+                              <!-- Messages: style can be found in dropdown.less-->
+                              <li class="dropdown messages-menu">
+                                <!-- Menu toggle button -->
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                  <i class="fa fa-envelope-o"></i>
+                                  <span class="label label-success">4</span>
                                 </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
+                                <ul class="dropdown-menu">
+                                  <li class="header">You have 4 messages</li>
+                                  <li>
+                                    <!-- inner menu: contains the messages -->
+                                    <ul class="menu">
+                                      <li><!-- start message -->
+                                        <a href="#">
+                                          <div class="pull-left">
+                                            <!-- User Image -->
+                                            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                          </div>
+                                          <!-- Message title and timestamp -->
+                                          <h4>
+                                            Support Team
+                                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                                          </h4>
+                                          <!-- The message -->
+                                          <p>Why not buy a new awesome theme?</p>
                                         </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
+                                      </li>
+                                      <!-- end message -->
+                                    </ul>
+                                    <!-- /.menu -->
+                                  </li>
+                                  <li class="footer"><a href="#">See All Messages</a></li>
                                 </ul>
-                            </li>
+                              </li>
+                              <!-- /.messages-menu -->
+
+                              <!-- Notifications Menu -->
+                              <li class="dropdown notifications-menu">
+                                <!-- Menu toggle button -->
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                  <i class="fa fa-bell-o"></i>
+                                  <span class="label label-warning">10</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                  <li class="header">You have 10 notifications</li>
+                                  <li>
+                                    <!-- Inner Menu: contains the notifications -->
+                                    <ul class="menu">
+                                      <li><!-- start notification -->
+                                        <a href="#">
+                                          <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                        </a>
+                                      </li>
+                                      <!-- end notification -->
+                                    </ul>
+                                  </li>
+                                  <li class="footer"><a href="#">View all</a></li>
+                                </ul>
+                              </li>
+                              <!-- Tasks Menu -->
+                              <li class="dropdown tasks-menu">
+                                <!-- Menu Toggle Button -->
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                  <i class="fa fa-flag-o"></i>
+                                  <span class="label label-danger">9</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                  <li class="header">You have 9 tasks</li>
+                                  <li>
+                                    <!-- Inner menu: contains the tasks -->
+                                    <ul class="menu">
+                                      <li><!-- Task item -->
+                                        <a href="#">
+                                          <!-- Task title and progress text -->
+                                          <h3>
+                                            Design some buttons
+                                            <small class="pull-right">20%</small>
+                                          </h3>
+                                          <!-- The progress bar -->
+                                          <div class="progress xs">
+                                            <!-- Change the css width attribute to simulate progress -->
+                                            <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                                              <span class="sr-only">20% Complete</span>
+                                            </div>
+                                          </div>
+                                        </a>
+                                      </li>
+                                      <!-- end task item -->
+                                    </ul>
+                                  </li>
+                                  <li class="footer">
+                                    <a href="#">View all tasks</a>
+                                  </li>
+                                </ul>
+                              </li>
+                              <!-- User Account Menu -->
+                              <li class="dropdown user user-menu">
+                                <!-- Menu Toggle Button -->
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                  <!-- The user image in the navbar-->
+                                  <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                                  <!-- hidden-xs hides the username on small devices so only the image appears. -->
+                                  <span class="hidden-xs"> {{ Auth::user()->name }}</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                  <!-- The user image in the menu -->
+                                  <li class="user-header">
+                                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+
+                                    <p>
+                                      Alexander Pierce - Web Developer
+                                      <small>Member since Nov. 2012</small>
+                                    </p>
+                                  </li>
+                                  <!-- Menu Body -->
+                                  <li class="user-body">
+                                    <div class="row">
+                                      <div class="col-xs-4 text-center">
+                                        <a href="#">Followers</a>
+                                      </div>
+                                      <div class="col-xs-4 text-center">
+                                        <a href="#">Sales</a>
+                                      </div>
+                                      <div class="col-xs-4 text-center">
+                                        <a href="#">Friends</a>
+                                      </div>
+                                    </div>
+                                    <!-- /.row -->
+                                  </li>
+                                  <!-- Menu Footer-->
+                                  <li class="user-footer">
+                                    <div class="pull-left">
+                                      <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                      <a href="{{ route('logout') }}"  onclick="event.preventDefault();
+                                                                         document.getElementById('logout-form').submit();" class="btn btn-default btn-flat">Sign out</a>
+                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                                {{ csrf_field() }}
+                                                            </form>                                                     
+                                    </div>
+                                  </li>
+                                </ul>
+                              </li>
+                            </ul>
+                          </div>
+                        </nav>
+                        </header>
                         @endif
                     </ul>
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
+
     <div class="footer">
         Copyright HRM System @2017
     </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
